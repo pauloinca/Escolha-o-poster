7c5b98e6f4eec3616de17c48a38995d5a11c9ae0
pauloinca <paulohincar@gmail.com>
2021-10-27 00:01:24 -0300

    Add cards

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,14 @@ import { CommonModule } from '@angular/common';
 import { Routes, RouterModule } from '@angular/router'; // CLI imports router
 import { HomeComponent } from './home/home.component';
 
-const routes: Routes = [
-  { path: '', component: HomeComponent },  
-  { path: 'home-component', component: HomeComponent },  
+const routes: Routes = [  
+  { path: 'home', component: HomeComponent },  
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
 ];
 
 @NgModule({
   declarations: [],
-  imports: [ RouterModule.forRoot(routes) ],
-  exports: [ RouterModule ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
 })
 export class AppRoutingModule { }
