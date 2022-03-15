74383a891045a0e29356a3464a3615c107a6ccb8
pauli <pauli@DESKTOP-0RO9D32>
2022-03-15 12:39:38 -0300

    Fix naming in Home Component

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { TmdbService } from '../services/tmdb.service';
 import { Movie } from '../models/movie';
-import { timer, interval } from 'rxjs';
+import { timer, interval, Observable } from 'rxjs';
 import { switchMap } from 'rxjs/operators';
 import { animate, state, style, transition, trigger } from '@angular/animations';
 
@@ -31,16 +31,15 @@ import { animate, state, style, transition, trigger } from '@angular/animations'
 export class HomeComponent implements OnInit {
 
   movie = {} as Movie;
-  image1Src: string = "../../assets/images/image1.png";
-  image2Src: string = "../../assets/images/image1.png";
-  // movies: Movie;
+  imageEsquerdaSrc: string = "../../assets/images/imageEsquerda.png";
+  imageDireitaSrc: string = "../../assets/images/imageEsquerda.png";
 
-  data1 = {
+  cardEsquerda = {
     state: "flipped"
   };
 
-  data2 = {
-    state: "default"
+  cardDireita = {
+    state: "flipped"
   };
 
   constructor(private tmdbService: TmdbService) { }
@@ -50,75 +49,43 @@ export class HomeComponent implements OnInit {
     return Math.floor(Math.random() * (max - min + 1)) + min;
   }
 
-  async card1Clicked() {
-    if (this.data1.state === "flipped") {
-      this.data1.state = "default";
-      await this.getPoster1();
-      setTimeout(() => this.data1.state = "flipped", 200);
-      // this.data1.state = "flipped";
-      // this.data1.state = "default";
+  async cardEsquerdaClicked() {
+    if (this.cardDireita.state == "flipped") {
+      this.cardDireita.state = "default";
+      await this.getOnePoster().then(poster => {
+        this.imageDireitaSrc = poster;
+      });
+
+      setTimeout(() => this.cardDireita.state = "flipped", 400);
     }
   }
 
-  card2Clicked() {
+  async cardDireitaClicked() {
+    if (this.cardEsquerda.state == "flipped") {
+      this.cardEsquerda.state = "default";
+      await this.getOnePoster().then(poster => {
+        this.imageEsquerdaSrc = poster;
+      });
 
+      setTimeout(() => this.cardEsquerda.state = "flipped", 400);
+    }
   }
 
-  async getPoster1() {
-    return this.tmdbService.getMovie().then((x: Movie[]) => {
-      this.image1Src = `https://image.tmdb.org/t/p/w500${x[0].poster_path}`;
-    });
-  }
-
-  getPoster2() {
-    this.tmdbService.getMovie().then((x: Movie[]) => {
-      this.image1Src = `https://image.tmdb.org/t/p/w500${x[1].poster_path}`;
+  async getOnePoster(): Promise<any> {
+    return this.tmdbService.getOneMovie().then((x: Movie) => {
+      return `https://image.tmdb.org/t/p/w500${x.poster_path}`;
     });
   }
 
   getAllPosters() {
-    this.tmdbService.getMovie().then((x: Movie[]) => {
-      this.image1Src = `https://image.tmdb.org/t/p/w500${x[0].poster_path}`;
-      this.image2Src = `https://image.tmdb.org/t/p/w500${x[1].poster_path}`;
+    this.tmdbService.getTwoMovies().then((x: Movie[]) => {
+      this.imageEsquerdaSrc = `https://image.tmdb.org/t/p/w500${x[0].poster_path}`;
+      this.imageDireitaSrc = `https://image.tmdb.org/t/p/w500${x[1].poster_path}`;
     });
   }
 
   ngOnInit() {
     this.getAllPosters();
-
-
-
-
-    // this.tmdbService.getMovie().pipe(switchMap(x => {
-    //   console.log("oi");
-    //   return x.poster_path;
-    // }))
-    // .subscribe(y => {
-    //   console.log(y);
-    // })
-
-    // this.tmdbService.getMovie().subscribe((movie: Movie) => {
-    //   this.movie = movie;  
-    //   this.imageSrc = `https://image.tmdb.org/t/p/w500${movie.poster_path}`;    
-    // })
-
-
-    // console.log(this.movie.id);
-
-    // timer(1000).subscribe(x => {  })    
-
   }
-
-  // // Chama o serviço que obtem o ultimo filme
-  // getLatestMovie(){
-  //   this.tmdbService.getLatestMovieId().subscribe((movie: Movie) => {      
-  //     this.movie = movie;
-  //     var randomId = this.randomInteger(0, this.movie.id);
-  //       this.tmdbService.getMovie(randomId, this.movie.id).subscribe((movie: Movie) => {          
-  //         console.log(movie);
-  //       });
-  //     // console.log(this.movie.id);
-  //   });
-  // }
 
 }
